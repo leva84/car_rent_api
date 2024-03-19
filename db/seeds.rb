4b10ac5bc6c71893f66fe829757772ed2eebd81f
DenisLevchenko <denfrut8@gmail.com>
2024-03-20 00:20:49 +0300

    Added seeds for user, car, rental

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Создаем 100 пользователей
+100.times do
+  User.create!(name: Faker::Name.name)
+end
+
+# Создаем 100 автомобилей
+100.times do
+  Car.create!(name: Faker::Vehicle.make_and_model)
+end
+
+# Создаем аренду для 30% пользователей и автомобилей
+users = User.all.sample((User.count * 0.3).to_i)
+cars = Car.all.sample((Car.count * 0.3).to_i)
+
+users.zip(cars).each { |user, car| Rental.create!(car: car, user: user) }
