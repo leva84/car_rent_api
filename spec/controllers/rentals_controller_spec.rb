997b1d1d103dbb2e2e8e7bd8fe23d004ae09f971
DenisLevchenko <denfrut8@gmail.com>
2024-03-20 15:46:18 +0300

    Update tests and refactoring

--- spec/controllers/rentals_controller_spec.rb
+++ spec/controllers/rentals_controller_spec.rb
@@ -5,26 +5,78 @@ describe RentalsController, type: :controller do
   describe 'POST #start_rental' do
     subject { post :start_rental, params: { user_id: user.id, car_id: car.id } }
 
-    it 'returns http status ok' do
-      expect(subject).to have_http_status(:ok)
+    shared_examples :start_rental_call do
+      it 'calls RentalService#start_rental' do
+        expect_any_instance_of(RentalService).to receive(:start_rental)
+        subject
+      end
     end
 
-    it 'calls RentalService#start_rental' do
-      expect_any_instance_of(RentalService).to receive(:start_rental)
-      subject
+    context 'with valid user and car' do
+      it 'returns http status ok' do
+        expect(subject).to have_http_status(:ok)
+      end
+
+      include_examples :start_rental_call
+    end
+
+    context 'with invalid user' do
+      let!(:rental) { create(:rental, user: user) }
+
+      it 'returns http status unprocessable_entity' do
+        expect(subject).to have_http_status(:unprocessable_entity)
+      end
+
+      include_examples :start_rental_call
+    end
+
+    context 'with invalid car' do
+      let(:car) { create :car, status: Car::MAINTENANCE }
+
+      it 'returns http status unprocessable_entity' do
+        expect(subject).to have_http_status(:unprocessable_entity)
+      end
+
+      include_examples :start_rental_call
+    end
+
+    context 'with rental' do
+      let!(:rental) { create(:rental, user: user, car: car) }
+
+      it 'returns http status ok' do
+        expect(subject).to have_http_status(:ok)
+      end
+
+      include_examples :start_rental_call
     end
   end
 
   describe 'DELETE #end_rental' do
     subject { delete :end_rental, params: { user_id: user.id, car_id: car.id } }
 
-    it 'returns http status ok' do
-      expect(subject).to have_http_status(:ok)
+    shared_examples :end_rental_call do
+      it 'calls RentalService#start_rental' do
+        expect_any_instance_of(RentalService).to receive(:end_rental)
+        subject
+      end
     end
 
-    it 'calls RentalService#end_rental' do
-      expect_any_instance_of(RentalService).to receive(:end_rental)
-      subject
+    context 'with rental' do
+      let!(:rental) { create(:rental, user: user, car: car) }
+
+      it 'returns http status ok' do
+        expect(subject).to have_http_status(:ok)
+      end
+
+      include_examples :end_rental_call
+    end
+
+    context 'without rental' do
+      it 'returns http status unprocessable_entity' do
+        expect(subject).to have_http_status(:unprocessable_entity)
+      end
+
+      include_examples :end_rental_call
     end
   end
 end
--- spec/services/rental_service_spec.rb
+++ spec/services/rental_service_spec.rb
@@ -2,17 +2,30 @@ describe RentalService, type: :service do
   let(:user) { create(:user) }
   let(:car) { create(:car) }
   let(:service) { described_class.new(car: car, user: user) }
+  let(:statuses) { Car.statuses.invert }
+  let(:available) { Car::AVAILABLE }
+  let(:rented) { Car::RENTED }
 
   describe '#start_rental' do
+    it 'creates rental' do
+      expect { service.start_rental }.to change { Rental.count }.by(1)
+    end
+
     it 'changes car status to rented' do
-      expect { service.start_rental }.to change { car.reload.status }.from('available').to('rented')
+      expect { service.start_rental }.to change { car.reload.status }.from(statuses[available]).to(statuses[rented])
     end
   end
 
   describe '#end_rental' do
+    before { service.start_rental }
+
     it 'changes car status to available' do
       service.start_rental
-      expect { service.end_rental }.to change { car.reload.status }.from('rented').to('available')
+      expect { service.end_rental }.to change { car.reload.status }.from(statuses[rented]).to(statuses[available])
+    end
+
+    it 'deletes rental' do
+      expect { service.end_rental }.to change { Rental.count }.by(-1)
     end
   end
 end
